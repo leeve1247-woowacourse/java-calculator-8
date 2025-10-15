7d6659c135c75330f560b99ff1dc9f3ee5698cd1
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-10-15 15:54:57 +0900

    refactor(test): NonAsciiCharacters에 대한 경고 표시 삭제

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -9,6 +9,7 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@SuppressWarnings("NonAsciiCharacters")
 class ApplicationTest extends NsTest {
     @Test
     void 커스텀_구분자_사용() {
